ab17510bc9fdd15d10f7a2a004d4c968f3f15353
Rohit <muhammadizroil22@gmail.com>
2024-03-20 10:37:34 +0700

    final create layout detail and activity detail

--- app/src/main/java/com/catnip/layoutingexample/feature/detail/DetailActivity.kt
+++ app/src/main/java/com/catnip/layoutingexample/feature/detail/DetailActivity.kt
@@ -7,17 +7,29 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.catnip.layoutingexample.model.Food
 import com.catnip.layoutingexample.databinding.ActivityDetailBinding
+import com.catnip.layoutingexample.databinding.LayoutDetailBannerBinding
+import com.catnip.layoutingexample.databinding.LayoutDetailContentBinding
+import com.catnip.layoutingexample.databinding.LayoutDetailFoodBinding
+import com.catnip.layoutingexample.databinding.LayoutDetailLocationBinding
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
+    private lateinit var bannerBinding: LayoutDetailBannerBinding
+    private lateinit var contentBinding: LayoutDetailContentBinding
+    private lateinit var foodBinding: LayoutDetailFoodBinding
+    private lateinit var locationBinding: LayoutDetailLocationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
+        bannerBinding = binding.layoutDetailBanner
+        contentBinding = binding.layoutDetailContent
+        foodBinding = binding.layoutDetailFood
+        locationBinding = binding.layoutDetailLocation
         setContentView(binding.root)
 
-        binding.btnBack.setOnClickListener {
-            onBackPressed() // Kembali ke tata letak sebelumnya saat tombol "Back" ditekan
+        bannerBinding.btnBack.setOnClickListener {
+            onBackPressed()
         }
 
         val food = intent.getParcelableExtra<Food>(EXTRAS_ITEM)
@@ -25,20 +37,24 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun displayFoodDetails(food: Food) {
-        binding.apply {
+        foodBinding.apply {
             tvNameFood.text = food.name
             tvPrice.text = food.price
             tvDetailsFood.text = food.detailfood
+        }
+        locationBinding.apply {
             tvLocation.text = food.location
             tvDetailLocation.text = food.detailLocation
-            Glide.with(this@DetailActivity)
-                .load(food.imagefoodPictUrl)
-                .into(ivProvile)
             tvDetailLocation.setOnClickListener {
                 openLocationUrl(food.locationPictUrl)
             }
         }
+        bannerBinding.apply {
+            Glide.with(this@DetailActivity)
+                .load(food.imagefoodPictUrl)
+                .into(ivProvile)}
     }
+
 
     private fun openLocationUrl(url: String) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
@@ -47,6 +63,5 @@ class DetailActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRAS_ITEM = "EXTRAS_ITEM"
-
     }
 }
